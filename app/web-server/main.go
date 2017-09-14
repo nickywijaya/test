6f1fced8b69bc218b7c3da2a35fe9c1b9829cab3
Indra Saputra <flame.fire99@gmail.com>
2017-09-14 14:51:13 +0700

    call mysql connection in main.go

--- app/web-server/main.go
+++ app/web-server/main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/bukalapak/packen/middleware"
 	"github.com/julienschmidt/httprouter"
+	"github.com/subosito/gotenv"
 
 	gx "github.com/bukalapak/go-xample"
+	"github.com/bukalapak/go-xample/database"
 	"github.com/bukalapak/go-xample/handler"
 )
 
 func main() {
-	gX := gx.NewGoXample()
+	gotenv.Load("../../.env")
+
+	dbOpt := database.Option{
+		User:     os.Getenv("MYSQL_USER"),
+		Password: os.Getenv("MYSQL_PASSWORD"),
+		Host:     os.Getenv("MYSQL_HOST"),
+		Port:     os.Getenv("MYSQL_PORT"),
+		Database: os.Getenv("MYSQL_DATABASE"),
+		Charset:  os.Getenv("MYSQL_CHARSET"),
+	}
+
+	mysql, _ := database.NewMySQL(dbOpt)
+	gX := gx.NewGoXample(mysql)
 	gxHandler := handler.NewHandler(gX)
 
 	router := httprouter.New()
